5a6e5be64a4f113a315720e8d4584a910d4cd287
mhhong <mh.hong@proton.me>
2026-05-24 02:14:00 +0900

    Add the root endpoint & introduce pydantic basemodel for chat request and response

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException, Header
 from openai import OpenAI
+from pydantic import BaseModel
 import os
 from dotenv import load_dotenv
 
@@ -9,7 +10,7 @@ API_KEY_CREDITS = {os.getenv("OPENAI_API_KEY"): 5}
 
 app = FastAPI()
 
-llm = OpenAI()
+
 
 
 def verify_api_key(x_api_key: str = Header(None)):
@@ -21,19 +22,44 @@ def verify_api_key(x_api_key: str = Header(None)):
     return x_api_key
 
 
-@app.post("/talk")
-def talk(prompt: str, x_api_key: str = Depends(verify_api_key)):
+def load_system_prompt():
+    try:
+        with open("prompts/system_prompt.md") as f:
+            return f.read()
+    except FileNotFoundError:
+        return None
 
-    print(f"USER PROMPT: {prompt}")
+system_prompt = load_system_prompt()
+llm = OpenAI()
+
+
+# Pydantic Models
+class ChatRequest(BaseModel):
+    prompt: str
+
+class ChatResponse(BaseModel):
+    response: str
+
+
+# API Endpoints
+@app.post("/chat", response_model=ChatResponse)
+def talk(request: ChatRequest, x_api_key: str = Depends(verify_api_key)):
+
+    print(f"USER PROMPT: {request.prompt}")
     API_KEY_CREDITS[x_api_key] -= 1
 
     response = llm.responses.create(
         model="gpt-5-mini-2025-08-07",
-        instructions="You are a helpful assistant",
-        input=prompt
+        instructions=system_prompt, # Newer version -- system_prompt
+        input=request.prompt # Newer version -- user_prompt
     )
     print(f"AI: {response.output_text}")
 
     return {
         "response": response.output_text
     }
+
+
+@app.get("/")
+def root():
+    return {"message": "API is running -- Homepage"}
