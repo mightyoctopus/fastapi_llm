47f3903385dc82fe4542707ea8c72e8b2711ddb4
mhhong <mh.hong@proton.me>
2026-05-24 03:08:59 +0900

    Modularize LLM for separation of concerns

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Depends, HTTPException, Header
-from openai import OpenAI
 from pydantic import BaseModel
 import os
 from dotenv import load_dotenv
+
+from ai.gpt import Gpt
 
 load_dotenv()
 # Set credit remaining -- {api_key: credit_num}
@@ -14,6 +15,8 @@ app = FastAPI()
 
 
 def verify_api_key(x_api_key: str = Header(None)):
+    # If the api key matches the actual api key, then the credits var holds the value.
+    # Otherwise, the api key is wrong, then it becomes 0 (This one also includes the error handling for wrong API key)
     credits = API_KEY_CREDITS.get(x_api_key, 0)
     print("CREDITS REMAINING: ", credits)
 
@@ -30,7 +33,7 @@ def load_system_prompt():
         return None
 
 system_prompt = load_system_prompt()
-llm = OpenAI()
+
 
 
 # Pydantic Models
@@ -48,15 +51,11 @@ def talk(request: ChatRequest, x_api_key: str = Depends(verify_api_key)):
     print(f"USER PROMPT: {request.prompt}")
     API_KEY_CREDITS[x_api_key] -= 1
 
-    response = llm.responses.create(
-        model="gpt-5-mini-2025-08-07",
-        instructions=system_prompt, # Newer version -- system_prompt
-        input=request.prompt # Newer version -- user_prompt
-    )
-    print(f"AI: {response.output_text}")
+    llm = Gpt(x_api_key, system_prompt)
+    response = llm.chat(request.prompt)
 
     return {
-        "response": response.output_text
+        "response": response
     }
 
 
